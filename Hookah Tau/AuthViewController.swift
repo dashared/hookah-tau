791cb49fa399078c55627d40b7d25785214cfcb3
Dasha Rednikina <dashared@yandex-team.ru>
2019-08-27 22:39:17 +0300

    Initial Commit

---
+++ Hookah Tau/AuthViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Hookah Tau
+//
+//  Created by Daria Rednikina on 27/08/2019.
+//  Copyright © 2019 Daria Rednikina. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
